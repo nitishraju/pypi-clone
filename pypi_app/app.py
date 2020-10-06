d1a09b450fb3a6cc3132be96bf1a57eb6ef02e58
Nitish Raju <nitishraju@outlook.com>
2020-10-05 21:30:51 -0400

    Added services folder for later database integration

---
+++ pypi_app/app.py
@@ -0,0 +1,18 @@
+import flask
+
+import services.package_service as package_service
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    test_packages = package_service.get_latest_packages()
+    return flask.render_template('home/index.html', packages=test_packages)
+
+@app.route('/about')
+def about():
+    return flask.render_template('home/about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ pypi_app/services/package_service.py
@@ -0,0 +1,6 @@
+def get_latest_packages():
+    return [
+        {'name': 'flask','version': '1.2.3'},
+        {'name': 'flask2','version': '1.2.5'},
+        {'name': 'flask5','version': '1.67.3'}
+    ]
