6a07fa833a49e7555011e5733c39e840ad5d1546
Nitish Raju <nitishraju@outlook.com>
2020-10-11 16:06:36 -0400

    Added a CMS for items from database

--- pypi_app/app.py
+++ pypi_app/app.py
@@ -7,13 +7,14 @@ sys.path.insert(0, folder)
 import pypi_app.views.home_views as home_views
 import pypi_app.views.package_views as package_views
 import pypi_app.views.account_views as account_views
+import pypi_app.views.cms_views as cms_views
 
 app = flask.Flask(__name__)
 
 app.register_blueprint(home_views.blueprint)
 app.register_blueprint(package_views.blueprint)
 app.register_blueprint(account_views.blueprint)
-
+app.register_blueprint(cms_views.blueprint)
 
 if __name__ == '__main__':
     print(app.url_map)
---
+++ pypi_app/services/cms_service.py
@@ -0,0 +1,22 @@
+fake_db = {
+    '/team/members/': {
+        'page_title': 'Our Members',
+        'page_details': 'Details about our members...',
+    },
+
+    '/team/goals/': {
+        'page_title': 'Our Goals',
+        'page_details': 'Details about our goals...',
+    },
+}
+
+def get_page(url: str) -> dict:
+    if not url:
+        return {}
+
+    url = url.lower().strip()
+    url = '/' + url.lstrip('/')
+
+    page=fake_db.get(url, {})
+    return page
+
