a9fa9a92b20531b9e6e652330a99f842aac67f40
Nitish Raju <nitishraju@outlook.com>
2020-11-25 00:22:26 -0500

    Added more 'fake' packages for testing

--- pypi_app/services/package_service.py
+++ pypi_app/services/package_service.py
@@ -1,6 +1,33 @@
 def get_latest_packages():
     return [
-        {'name': 'flask','version': '1.2.3'},
-        {'name': 'flask2','version': '1.2.5'},
-        {'name': 'flask5','version': '1.67.3'}
+        {'name': 'mxnet-cu100','version': '1.7.0',
+         'desc': "MXNet is an ultra-scalable deep learning framework. This version uses CUDA-10.0."},
+
+        {'name': 'pyahocorasick','version': '1.4.0',
+         'desc': "pyahocorasick is a fast and memory efficient library for exact or approximate multi-pattern string search."},
+
+        {'name': 'act-workers','version': '1.0.44',
+         'desc': "ACT workers"},
+
+        {'name': 'pykalman', 'version': '0.9.5',
+         'desc': "An implementation of the Kalman Filter, Kalman Smoother, and EM algorithm in Python"},
+
+        {'name': 'ipaddr', 'version': '2.2.0',
+         'desc': "Google's IP address manipulation library"}
+    ], \
+    [
+        {'name': 'faspy', 'version': '0.0.68',
+         'desc': "A python package for financial instruments"},
+
+        {'name': 'ppx', 'version': '0.5.0',
+         'desc': "Wrapper for Proxycheck"},
+
+        {'name': 'MLlego', 'version': '0.31',
+         'desc': "An RPC client library for the Transmission BitTorrent client"},
+
+        {'name': 'fastapi-plus', 'version': '0.1.4.20201125',
+         'desc': "An extender for aiogram to make it more OOP"},
+
+        {'name': 'mhcgnomes', 'version': '2.5.0',
+         'desc': "Python library for parsing MHC nomenclature in the wild"}
     ]
